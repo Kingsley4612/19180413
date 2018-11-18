afe95a3ed3dd1a18304b0b256cdee525a8364264
Kingsley4612 <752775649@qq.com>
2018-11-18 21:34:59 +0800

    Add homework-19180413

--- 20181113/16/16.c
+++ 20181113/16/16.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-   FILE *pSourceFile, *pDestFile;
-   char fSourceName[100], fDestName[100];
-   int ch;
-   scanf("%s", fSourceName);
-   scanf("%s", fDestName);
-
-   if ((pSourceFile = fopen(fSourceName, "r")) == NULL)
-   {
-       printf("打不开源文件！\n");
-       exit(1);
-   }
-   if ((pDestFile = fopen(fDestName,"w")) == NULL)
-   {
-       printf("打不开新文件！\n");
-       exit(1);
-   }
-
-   ch= fgetc(pSourceFile);
-   while (feof(pSourceFile)==0)
-   {
-       fputc(ch,pDestFile);
-       ch=fgetc(pSourceFile);
-   }
-   fclose(pSourceFile);
-   fclose(pDestFile);
+int main(int argc ,char *argv[]) {
+    FILE *pSourceFile,*pDestFile;
+    int ch,len;
+    char sourcename[80],destname[80],fname[20],name[20];
+strcpy(sourcename,argv[1]);
+strcpy(destname,argv[2]);
+len=strlen(sourcename);
+int i,j;
+for(i=len-1,j=0;sourcename[i]!='/';i--,j++)
+{
+   fname[j]=sourcename[i];
+}
+int len1=strlen(fname);
+name[0]='/';
+for(i=len1-1,j=1;i>=0;i--,j++)
+{
+     name[j]=fname[i];
+}
+strcat(destname,name);
+    if((pSourceFile=fopen(argv[1],"r"))==NULL)
+    {
+        printf("失败");
+        exit(1);
+    }
+    if((pDestFile=fopen(destname,"w"))==NULL)
+    {
+        printf("失败");
+        exit(1);
+    }
+    ch=fgetc(pSourceFile);
+    while(feof(pSourceFile)==0)
+    {
+        fputc(ch,pDestFile);
+        ch=fgetc(pSourceFile);
+    }
+    fclose(pSourceFile);
+    fclose(pDestFile);
     return 0;
 }
